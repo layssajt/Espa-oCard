c539ef3e29c1461a7564f9593e85bb040d6ce07b
layssajt <168855316+layssajt@users.noreply.github.com>
2024-11-29 19:46:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Planetas',
+    'Qual o maoir planeta do Sistema Solar?',
+    'O maior planeta é Júpiter.'
+)
+
+criaCartao(
+    'Planetas',
+    'Qual o planeta mais quente do Sistema Solar?',
+    'O planeta mais quente é Mercúrio.'
+)
+
+criaCartao(
+    'Planetas',
+    'Quantos planetas temos no Sistema Solar?',
+    'O Sistema Solar é formado por oito planetas: Mercúrio, Vênus, Terra, Marte, Júpiter, Saturno, Urano e Netuno.'
+)
+
+criaCartao(
+    'Curiosidade',
+    ' Qual a maior constelação reconhecida pela IAU?',
+    'A maior constelação reconhecida é a HIDRA.'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Qual a constelação mais antiga do mundo?',
+    'Atrações do Touro é a mais antiga das constelações.'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Quanto custa para comprar uma estrela?',
+    'Uma estrela pode ir de R$ 250 até R$ 600, sendo o pacote com "estrelas mais brilhantes".'
+)
